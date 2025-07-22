65154323afad982e094eb3b1a8917da89d4e383c
Ryan Rousseau <ryan.rousseau@gmail.com>
2025-07-22 20:55:52 +0000

    Adding blue-green process

---
+++ template-scripts/kubernetes-blue-green/get-active-deployment.ps1
@@ -0,0 +1,7 @@
+$serviceName = $OctopusParameters["Template.Service.Name"]
+
+$deploymentId = $(kubectl get service $serviceName-o json | jq -r '.spec.selector.deployment')
+
+Write-Verbose "Found previous deployment: $deploymentId"
+
+Set-OctopusVariable -name "PreviousDeployment" -value $deployment
