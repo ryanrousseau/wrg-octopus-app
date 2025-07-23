f6bc3a2904368e73b423c3d93df37468875f66c4
Ryan Rousseau <ryan.rousseau@gmail.com>
2025-07-23 21:47:20 +0000

    Update bg pattern

--- template-scripts/kubernetes-blue-green/get-active-deployment.ps1
+++ template-scripts/kubernetes-blue-green/get-active-deployment.ps1
@@ -2,6 +2,13 @@ $serviceName = $OctopusParameters["Template.Service.Name"]
 
 $deploymentId = $(kubectl get service $serviceName -o json | jq -r '.spec.selector.deployment')
 
-Write-Verbose "Found previous deployment: $deploymentId"
+$foundActiveDeployment = -not ($deploymentId -eq $null)
 
-Set-OctopusVariable -name "PreviousDeployment" -value $deploymentId
+if ($foundActiveDeployment) {
+    Write-Verbose "Found previous deployment: $deploymentId"
+    Set-OctopusVariable -name "PreviousDeployment" -value $deploymentId
+} else {
+    Write-Warning "Did not find an existing deployment"
+}
+
+Set-OctopusVariable -name "FoundActiveDeployment" -value $foundActiveDeployment
