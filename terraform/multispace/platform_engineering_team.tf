9de5fd49533a5f78303362b3d002314c53b11e44
Ryan Rousseau <ryan.rousseau@gmail.com>
2025-08-01 20:31:48 +0000

    Add more users

--- terraform/multispace/platform_engineering_team.tf
+++ terraform/multispace/platform_engineering_team.tf
@@ -7,16 +7,10 @@ module "platform_engineering_team" {
 
   name        = "Platform Engineering Team"
   description = "Global team representing the Platform Engineering team"
-  #external_security_groups = [
-  #    {
-  #        id = "externalGroupId"
-  #        display_name = "External Group Name"
-  #        display_id_and_name = true
-  #    }
-  #]
   users = [
     data.octopusdeploy_users.octopus_config_service_account.users[0].id,
     data.octopusdeploy_users.ryan_rousseau.users[0].id,
-    data.octopusdeploy_users.mark_harrison.users[0].id
+    data.octopusdeploy_users.mark_harrison.users[0].id,
+    data.octopusdeploy_users.grace_rehn.users[0].id
   ]
 }
--- terraform/multispace/users.tf
+++ terraform/multispace/users.tf
@@ -12,3 +12,28 @@ data "octopusdeploy_users" "mark_harrison" {
   filter = "Mark Harrison"
   take   = 1
 }
+
+data "octopusdeploy_users" "grace_rehn" {
+  filter = "Grace Rehn"
+  take   = 1
+}
+
+data "octopusdeploy_users" "ava" {
+  filter = "Ava Kamara"
+  take   = 1
+}
+
+data "octopusdeploy_users" "dillon" {
+  filter = "Dillon Taylor"
+  take   = 1
+}
+
+data "octopusdeploy_users" "ian" {
+  filter = "Ian James"
+  take   = 1
+}
+
+data "octopusdeploy_users" "ping" {
+  filter = "Ping Chen"
+  take   = 1
+}
