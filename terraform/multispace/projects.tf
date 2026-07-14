9ff67c00c9ff3a588098ecd14cc57295f96b888c
Ryan Rousseau <ryan.rousseau@gmail.com>
2026-07-14 15:04:14 +0000

    Add weather agent project data

--- terraform/multispace/projects.tf
+++ terraform/multispace/projects.tf
@@ -4,3 +4,11 @@ data "octopusdeploy_projects" "lambda_region_tenants" {
   take     = 1
   space_id = module.new_cascadia_imports_space.id
 }
+
+
+data "octopusdeploy_projects" "weather_agent_project" {
+  name     = "Weather Agent"
+  skip     = 0
+  take     = 1
+  space_id = module.new_cascadia_imports_space.id
+}
