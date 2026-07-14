08d09ac91e5750690fe9a586e85cb890fdef5548
Ryan Rousseau <ryan.rousseau@gmail.com>
2026-07-14 15:35:58 +0000

    Rollback to older tf provider

--- terraform/multispace/config.tf
+++ terraform/multispace/config.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     octopusdeploy = {
-      source = "OctopusDeploy/octopusdeploy", version = "1.18.0"
+      source = "OctopusDeploy/octopusdeploy", version = "1.6.0"
     }
   }
 }
